9411d3a50b67db9a939f8545107a32a66c8adfc2
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-18 10:04:53 +0300

    creating a session in the session_auth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,23 @@
 This module introduces a new class.
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     A class that inherits from the Auth class.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Class instance method that creates a session id
+        for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

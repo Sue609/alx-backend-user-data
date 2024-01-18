1bbc990ea8ffe66fa7a290b299b3d81a9e0112c8
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-18 09:49:34 +0300

    Creating an empty session_auth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module introduces a new class.
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    A class that inherits from the Auth class.
+    """
+    pass
+    

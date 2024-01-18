4cddb1ed247094d8f8c07739d912accc93a44453
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-18 20:58:53 +0300

    new class based on session ID stored in the database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.session_auth import SessionAuth
@@ -28,6 +28,8 @@ if auth_type == 'session_auth':
     auth = SessionAuth()
 if auth_type == 'session_exp_auth':
     auth = SessionExpAuth
+if auth_type == 'session_db_auth':
+    auth = SessionDBAuth
 
 
 @app.errorhandler(404)

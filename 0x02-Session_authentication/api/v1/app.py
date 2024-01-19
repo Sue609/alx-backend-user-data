7b7a1eadd90488d4f11506e3ef3db32a42a2bbe5
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-19 19:55:58 +0300

    improving the api/v1/app.py documentation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,16 +2,15 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
-from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
 
 

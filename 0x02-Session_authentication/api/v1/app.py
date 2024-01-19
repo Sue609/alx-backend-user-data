e492ff60baeb3dc8b3eff40a4d3e123312cac420
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-19 19:41:12 +0300

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -79,9 +79,6 @@ def authenticate_user():
 
 
 if __name__ == "__main__":
-    '''
-    setting the host and the port variables.
-    '''
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)

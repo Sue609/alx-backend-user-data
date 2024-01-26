3bdc7ed01625966cefc2f04403f8ceb85110ef13
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-26 11:33:04 +0300

    SQLAlchemy model named User

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,6 +5,7 @@ This module introduces a class.
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+
 Base = declarative_base()
 
 
@@ -12,10 +13,9 @@ class User(Base):
     """
     This class defines a table called users.
     """
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(350), nullable=True)
+    session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)

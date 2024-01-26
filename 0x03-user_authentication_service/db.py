9f6a6d6df15c059dc6f1b4f2f93f3fcb557c7971
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-26 12:18:25 +0300

    method takes in arbitrary keyword arguments and returns the first row found in the users table

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy import create_engine, tuple_
 
 from user import Base, User
 
@@ -51,10 +52,16 @@ class DB:
         Method takes in arbitrary keyword arguements and returne the
         first row found in the users table.
         """
-        try:
-            user = self.__session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound()
-            return user
-        except KeyError:
-            raise InvalidRequestError()
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
